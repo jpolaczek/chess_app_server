0cfe5fd569c72b1c7cafe4f1710154374280ae5a
jpolaczek <jakub.polaczek92@gmail.com>
2023-04-22 11:22:44 +0200

    validation rules for user model

--- src/models/zod/users.ts
+++ src/models/zod/users.ts
@@ -2,6 +2,6 @@ import * as z from "zod";
 
 export const UsersModel = z.object({
   id: z.string().uuid(),
-  name: z.string(),
-  password: z.string(),
+  name: z.string().min(7).max(20),
+  password: z.string().min(7).max(20),
 });
